0734c164e56a7ae98c728ff00c00da4d07ee5fe0
Mai13 <maialen.ber@hotmail.com>
2021-01-07 20:17:12 +0100

    BUG, method does not work for every sudoku

--- src/load_data.py
+++ src/load_data.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+import random
 
 
 class LoadData:
 
     def load_data(self, path, dataset):
-        data = pd.read_csv(f'{path}/{dataset}')
-        initial_matrix = np.array(list(map(int, list(data['puzzle'][0])))).reshape(9, 9)
-        solution = np.array(list(map(int, list(data['solution'][0])))).reshape(9, 9)
+        # data = pd.read_csv(f'{path}/{dataset}')
+        data = pd.read_csv(f'/Users/maialenberrondo/Desktop/sudoku.csv')
+        print(data.shape)
+        row_number = random.randint(0, 9000001)
+        print(row_number)
+        initial_matrix = np.array(list(map(int, list(data['puzzle'][row_number])))).reshape(9, 9)
+        solution = np.array(list(map(int, list(data['solution'][row_number])))).reshape(9, 9)
         return initial_matrix, solution
